598336d36dff9ca485099aa25b64ec4f62c71213
Asano-Naoki <asannaoki@gmail.com>
2023-06-03 20:21:49 +0900

    0612の宿題を改良

--- 0612.js
+++ 0612.js
@@ -1,14 +1,22 @@
 
 
-document.getElementById('ftoc').addEventListener('click', ()=>{
-    const f = document.getElementById('f').value;
-    const FtoC = (fDegree) => {
-        return (fDegree - 32) / 1.8;
-    }
-    const c = FtoC(Number(f));
-    result = '華氏' + f + '度は摂氏' + c + '度です。'
-    document.getElementById("ftoc-result").textContent = result;
-})
+// add-btnというidを持つHTML要素を取得し、定数に代入する
+const addBtn = document.getElementById('add-btn');
+// parent-listというidを持つHTML要素を取得し、定数に代入する
+const parentList = document.getElementById('parent-list');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+addBtn.addEventListener('click', () => {
+  // li要素を新しく作成する
+  const childList = document.createElement('li');
+
+  // 作成したli要素に「これはリスト要素です」というテキストを追加する
+  childList.textContent = 'この船に必要な検査は○△×です';
+
+  // 作成したli要素をul要素の子要素として末尾に追加する
+  parentList.appendChild(childList);
+});
+
 
 
 
